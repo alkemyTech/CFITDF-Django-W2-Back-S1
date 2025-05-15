85ba0fcf86614c7f14f536a45d1dedec14c27c65
caucot <welanicaucota@gmail.com>
2025-05-15 17:26:38 -0300

    Tarea 11: Configurar el Admin de ReservaServicio

--- src/app_reservaservicio/admin.py
+++ src/app_reservaservicio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ReservaServicio
 # Register your models here.
+
+@admin.register(ReservaServicio)
+class ReservaServicioAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'servicio', 'fecha_evento', 'fecha_reserva', 'empleado', 'coordinador')
+    list_filter = ('fecha_evento', 'servicio')
+    search_fields = ('cliente__nombre', 'cliente__apellido', 'servicio__nombre')
