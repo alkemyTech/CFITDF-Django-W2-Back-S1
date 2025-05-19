155ac021ad81ce2fa96d4ee2576d415a96ac2c23
caucot <welanicaucota@gmail.com>
2025-05-19 17:05:06 -0300

    Tarea 14: Implementar bajas logicas a Empleado

--- src/app_empleado/admin.py
+++ src/app_empleado/admin.py
@@ -6,3 +6,14 @@ class EmpleadoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'legajo', 'activo')
     search_fields = ('nombre', 'apellido')
     ordering = ('apellido', 'nombre')
+    
+    def get_queryset(self, request):
+        return Empleado.all_objects.all()
+
+    @admin.action(description='Dar de baja empleados seleccionados')
+    def dar_de_baja(self, request, queryset):
+        queryset.update(activo=False)
+
+    @admin.action(description='Dar de alta empleados seleccionados')
+    def dar_de_alta(self, request, queryset):
+        queryset.update(activo=True)
