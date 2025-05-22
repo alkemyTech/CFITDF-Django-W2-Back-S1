4ba673ba0ebf8b5f0201243095fdd07838ea63de
Ariel Voynes <voynes@gmail.com>
2025-05-22 16:57:26 -0300

    UPDATE para el modelo: Cliente

--- src/app_cliente/urls.py
+++ src/app_cliente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ClienteCreateView
+from .views import ClienteCreateView, ClienteUpdateView
 
 app_name = 'app_cliente'
 
 urlpatterns = [
     path('crear/', ClienteCreateView.as_view(), name='cliente_create'),
+    path('editar/<int:pk>/', ClienteUpdateView.as_view(), name='cliente_update'),
 ]
--- src/app_cliente/views.py
+++ src/app_cliente/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Cliente
 
@@ -12,4 +12,15 @@ class ClienteCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['model_name'] = 'nuevo cliente'
+        return context
+    
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    fields = ['nombre', 'apellido']
+    template_name = 'update.html'
+    success_url = reverse_lazy('app_cliente:cliente_update') 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_name'] = 'cliente'
         return context
