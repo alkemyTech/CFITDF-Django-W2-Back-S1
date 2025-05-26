f3fd1d6882e6645ec7116a438a43f487ef810309
Ariel Voynes <voynes@gmail.com>
2025-05-26 17:44:24 -0300

    READ para el modelo: Coordinador

--- src/app_coordinador/views.py
+++ src/app_coordinador/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from .models import Coordinador
 
@@ -12,4 +12,19 @@ class CoordinadorCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['model_name'] = 'nuevo coordinador'
+        return context
+    
+class CoordinadorListView(ListView):
+    model = Coordinador
+    template_name = "lista_generica.html"
+    context_object_name = "objetos" 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo"] = "Clientes"
+        context["encabezados_campos"] = [campo.verbose_name for campo in Coordinador._meta.fields] 
+        context["valores_campos"] = [
+            [getattr(obj, campo.name) for campo in Coordinador._meta.fields]
+            for obj in context["objetos"]
+        ]
         return context
