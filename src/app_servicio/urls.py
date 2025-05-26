b67060d5a31578f669bd0f86c5ef63f254981a1e
Ariel Voynes <voynes@gmail.com>
2025-05-26 17:27:29 -0300

    READ para el modelo: Servicio

--- src/app_servicio/urls.py
+++ src/app_servicio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ServicioCreateView
+from .views import ServicioCreateView, ServicioListView
 
 app_name = "app_servicio"
 
 urlpatterns = [
     path('crear/', ServicioCreateView.as_view(), name='servicio_create'),
+    path('', ServicioListView.as_view(), name='servicio_lista'),
 ]
--- src/app_servicio/views.py
+++ src/app_servicio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from .models import Servicio
 
@@ -12,4 +12,19 @@ class ServicioCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['model_name'] = 'nuevo servicio'
+        return context
+
+class ServicioListView(ListView):
+    model = Servicio
+    template_name = "lista_generica.html"
+    context_object_name = "objetos" 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo"] = "Servicios"
+        context["encabezados_campos"] = [campo.verbose_name for campo in Servicio._meta.fields] 
+        context["valores_campos"] = [
+            [getattr(obj, campo.name) for campo in Servicio._meta.fields]
+            for obj in context["objetos"]
+        ]
         return context
