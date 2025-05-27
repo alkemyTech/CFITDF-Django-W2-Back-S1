15cc5d608e4e9a6e6f262cdd39cbd6f2816b0b7f
caucot <welanicaucota@gmail.com>
2025-05-27 17:58:49 -0300

    Tarea 43: Api de detalles del servicio

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ServicioListaAPIView
+from .views import ServicioListaAPIView, ServicioRetrieveAPIView
 
 urlpatterns = [
     path('servicios/', ServicioListaAPIView.as_view(),
          name='api_servicios_lista'),
+    path('detalle/<int:pk>',ServicioRetrieveAPIView.as_view(),
+         name='api_servicios_detalle')
 ]
