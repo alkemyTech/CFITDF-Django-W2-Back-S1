27657f034b3541c41b29bc75efae029f84832d76
caucot <welanicaucota@gmail.com>
2025-05-27 18:37:42 -0300

    Tarea_44: Api de Cliente

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from app_servicio.models import Servicio
-
+from app_cliente.models import Cliente
 
 class ServicioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servicio
+        fields = '__all__'
+        
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
         fields = '__all__'
