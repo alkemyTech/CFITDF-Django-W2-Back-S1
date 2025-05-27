719c5c35935337be090c460d5fc319ba5f3fc07a
caucot <welanicaucota@gmail.com>
2025-05-27 18:10:49 -0300

    Tarea 43: Se cambio la ruta de detalle del servicio

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,6 +4,6 @@ from .views import ServicioListaAPIView, ServicioRetrieveAPIView
 urlpatterns = [
     path('servicios/', ServicioListaAPIView.as_view(),
          name='api_servicios_lista'),
-    path('detalle/<int:pk>',ServicioRetrieveAPIView.as_view(),
+    path('servicios/<int:pk>',ServicioRetrieveAPIView.as_view(),
          name='api_servicios_detalle')
 ]
