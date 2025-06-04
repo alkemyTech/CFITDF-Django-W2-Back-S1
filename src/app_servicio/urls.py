7fdbe0af52c41257a3e25723befa319762923f5b
Ariel Voynes <voynes@gmail.com>
2025-06-04 02:39:44 -0300

    Detalle para el modelo: Servicio

--- src/app_servicio/urls.py
+++ src/app_servicio/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
-    ServicioCreateView, ServicioListView, 
-    ServicioUpdateView, ServicioDeleteView
+    ServicioCreateView, ServicioListView,
+    ServicioUpdateView, ServicioDeleteView,
+    ServicioDetailView
 )
 
 app_name = "app_servicio"
@@ -9,8 +10,10 @@ app_name = "app_servicio"
 urlpatterns = [
     path('crear/', ServicioCreateView.as_view(), name='servicio_create'),
     path('', ServicioListView.as_view(), name='servicio_lista'),
-    path('editar/<int:pk>/', ServicioUpdateView.as_view(), 
+    path('editar/<int:pk>/', ServicioUpdateView.as_view(),
          name='servicio_update'),
-    path("borrar/<int:pk>/", ServicioDeleteView.as_view(), 
+    path('borrar/<int:pk>/', ServicioDeleteView.as_view(),
          name="borrar_servicio"),
+    path('detalles/<int:pk>/', ServicioDetailView.as_view(),
+         name="detalle_servicio"),
 ]
