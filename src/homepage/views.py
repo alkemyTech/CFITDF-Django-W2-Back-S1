c92329aa6107745ef8f1da03db684b1eec08e2cc
Ariel Voynes <voynes@gmail.com>
2025-06-08 23:47:57 -0300

    Crear instructivo de uso de Endpoints

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'homepage/home.html')
+
+def endpoints(request):
+    return render(request, 'homepage/endpoints.html')
